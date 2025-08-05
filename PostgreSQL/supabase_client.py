2c3cd33ea5f5e1a3c0b6d61663fe5007561e004f
Hussein <80421851+husseinaltahhan@users.noreply.github.com>
2025-08-05 14:47:13 +0300

    Got rid of exposed API key

--- PostgreSQL/supabase_client.py
+++ PostgreSQL/supabase_client.py
@@ -1,8 +1,8 @@
 from supabase import create_client, Client
 
 # Your Supabase credentials
-url: str = "https://cejlsybbdezcdmrgiqxv.supabase.co"
-key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNlamxzeWJiZGV6Y2RtcmdpcXh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM3Mjc2MDIsImV4cCI6MjA2OTMwMzYwMn0.OyYVvM0zM7q8rTKklYGYKVfv7cm8R4twODVy_zxOwcU"
+url: str = "https://.supabase.co"
+key: str = ""
 
 # Create the Supabase client
 supabase: Client = create_client(url, key)
